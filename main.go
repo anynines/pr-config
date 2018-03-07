6954a3ba3641f40df339736df1b70e82e27fac13
Robert Gogolok <gogolok@gmail.com>
2018-03-08 00:23:00 +0100

    basic auth

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/anynines/pr-config/web"
 
 func main() {
-	web.Run("4455")
+	web.Run("4455", "username", "password")
 }
--- web/web.go
+++ web/web.go
@@ -2,25 +2,31 @@ package web
 
 import "github.com/gin-gonic/gin"
 
-func Run(defaultPort string) {
+func Run(defaultPort string, username string, password string) {
 	r := gin.Default()
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"running": "true",
 		})
 	})
 
-	r.GET("/v1/:org/:project/:pr", func(c *gin.Context) {
-		_ = c.Param("org")
-		_ = c.Param("project")
-		_ = c.Param("pr")
+	authorized := r.Group("/v1", gin.BasicAuth(gin.Accounts{
+		username: password,
+	}))
 
-		c.JSON(200, gin.H{
-			"backup": true,
-			"cache":  false,
+	{
+		authorized.GET(":org/:project/:pr", func(c *gin.Context) {
+			_ = c.Param("org")
+			_ = c.Param("project")
+			_ = c.Param("pr")
+
+			c.JSON(200, gin.H{
+				"backup": true,
+				"cache":  false,
+			})
 		})
-
-	})
+	}
 
 	r.Run(":" + defaultPort)
 }
